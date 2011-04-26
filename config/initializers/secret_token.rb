077b60cf5ce69ea448236f6c90dc034f36e68b7e
Kristján Pétursson <kristjan@causes.com>
2011-04-25 20:51:06 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Greggers::Application.config.secret_token = '4386693280b113fdf52b9c2e37cd21c86f89c8d555cf3f7e2138c723d251be23a9dc8f9d5c23051be655755930e00373fe0bd933a141b3dd116e0597956fd687'
